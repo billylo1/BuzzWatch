7d9e994ff7aad21de3ddfab64e3e06b40000358b
Billy Lo <billylo1@gmail.com>
2022-09-27 17:23:04 -0400

    Initial Commit

---
+++ BuzzWatch Watch App/BuzzWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuzzWatchApp.swift
+//  BuzzWatch Watch App
+//
+//  Created by Billy Lo on 2022-09-27.
+//
+
+import SwiftUI
+
+@main
+struct BuzzWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BuzzWatch Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  BuzzWatch Watch App
+//
+//  Created by Billy Lo on 2022-09-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
